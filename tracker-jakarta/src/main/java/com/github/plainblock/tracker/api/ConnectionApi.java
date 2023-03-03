963a54122ee161a6e50f878dc1e556dd4606b0cf
plainblock <shohezaka.little@gmail.com>
2023-03-03 12:10:00 +0900

    Move produces annotation; method to class

--- tracker-jakarta/src/main/java/com/github/plainblock/tracker/api/ConnectionApi.java
+++ tracker-jakarta/src/main/java/com/github/plainblock/tracker/api/ConnectionApi.java
@@ -11,9 +11,9 @@ import jakarta.ws.rs.core.Response;
 import com.github.plainblock.tracker.usecase.ConnectionUsecase;
 import com.github.plainblock.tracker.usecase.input.ConnectionInput;
 import com.github.plainblock.tracker.usecase.output.ConnectionOutput;
-import com.github.plainblock.tracker.util.LogUtil;
 
 @Path("/api/v1/connections")
+@Produces(MediaType.APPLICATION_JSON)
 public class ConnectionApi {
 
 //    private static final Logger LOGGER = LoggerFactory.getLogger(ConnectionApi.class);
@@ -22,9 +22,7 @@ public class ConnectionApi {
     private ConnectionUsecase usecase;
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public Response fetchConnectionStatus(@QueryParam("host") String host) {
-//        LogUtil.loggingRequest(LOGGER, request);
         ConnectionInput input = new ConnectionInput(host);
         ConnectionOutput output = usecase.verifyConnection(input);
         return Response.ok(output).build();
