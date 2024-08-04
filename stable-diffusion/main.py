07b0e36d8d9ee130fd5c2bb689a263dbb72d162b
plainblock <shohezaka.little@gmail.com>
2024-08-05 01:27:39 +0900

    Change default model

--- stable-diffusion/main.py
+++ stable-diffusion/main.py
@@ -16,19 +16,25 @@ if __name__ == '__main__':
 
     # Add arguments
     parser.add_argument("-n", "--number", type=int, default=1, help="Number of images to generate")
-    parser.add_argument("-m", "--model", type=str, default="stabilityai/stable-diffusion-3-medium-diffusers",
-                        help="Model name")
+    parser.add_argument("-m", "--model", type=str, default="gsdf/Counterfeit-V2.5", help="Model name")
     parser.add_argument("-d", "--directory", type=str, default="generate", help="Save directory")
 
-    # Setup file and directory
+    # View arguments
     args = parser.parse_args()
-    positive, negative = load_prompt("positive-prompt.txt", "negative-prompt.txt")
-    os.makedirs(args.directory, exist_ok=True)
+    print("model: {}".format(args.model))
+    print("number: {}".format(args.number))
+    print("directory: {}".format(args.directory))
 
-    # Login to hugging face
+    # Read prompts
+    positive, negative = load_prompt("positive-prompt.txt", "negative-prompt.txt")
+    print("positive: {}".format(positive))
+    print("negative: {}".format(negative))
+
+    # Login to Hugging Face
     token = load_token("token.txt")
     login(token, add_to_git_credential=True)
 
     # Generate image
+    os.makedirs(args.directory, exist_ok=True)
     model = setup_model(args.model)
     generate_images(model, positive, negative, args.number, args.directory)
